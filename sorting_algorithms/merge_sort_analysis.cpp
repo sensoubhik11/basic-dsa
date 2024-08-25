19cf378b273ab4941ae11e210fd3bdcf211d3fa7
Soubhik Sen <ss.24p10223@nitdgp.ac.in>
2024-08-25 18:37:13 +0530

    Tracking the number of comparisons and merge calls in merge sort

--- sorting_algorithms/merge_sort_analysis.cpp
+++ sorting_algorithms/merge_sort_analysis.cpp
@@ -6,8 +6,12 @@ Roll : 24MA4108
 Algorithm Class Date: Aug 23, 2024
 Assignment : Implement and demonstrate merge sort
 */
+int n = 0;
+int COUNT_MERGE_CALLS = 0;
+int COUNT_COMPARISONS[1000];
+void print(int[], int);
 
-void merge(int a[], int l, int m, int h) {
+void merge(int a[], int l, int m, int h) {  
   //finding size of left and right halves
   int n1 = m-l+1;
   int n2 = h-m;
@@ -17,6 +21,8 @@ void merge(int a[], int l, int m, int h) {
   for(int i=0; i<n1; i++) left[i] = a[l+i];
   for(int j=0; j<n1; j++) right[j] = a[m+1+j];
   
+  std::cout << "\nBefore Merge:\n";
+  print(a,n);
   //now the actual merge will start
   //we will compare the elements of left and right halves
   //and put the minimum of the two in our actual array a[l..h]
@@ -31,17 +37,26 @@ void merge(int a[], int l, int m, int h) {
       j++;
     }
     k++;
+    COUNT_COMPARISONS[COUNT_MERGE_CALLS]++;
   }
   
   //if some elements are still there in any of left or right half
   while(i<n1) {
     a[k] = left[i];
     k++, i++;
+    COUNT_COMPARISONS[COUNT_MERGE_CALLS]++;
   }
   while(j<n2) {
     a[k] = right[j];
     k++, j++;
+    COUNT_COMPARISONS[COUNT_MERGE_CALLS]++;
   }
+  
+  std::cout << "After Merge:\n";
+  print(a,n);
+  std::cout << "Number of comparisons made in this merging: " << COUNT_COMPARISONS[COUNT_MERGE_CALLS] << "\n";
+  
+  COUNT_MERGE_CALLS++;
 }
 
 void mergeSort(int a[], int low, int high) {
@@ -59,7 +74,7 @@ void print(int a[], int n) {
 }
 
 int main() {
-  int n;
+
   std::cout << "Enter number of elements: ";
   std::cin >> n;
  
@@ -76,8 +91,16 @@ int main() {
   std::cout << "Merge Sorting the array...\n\n";
   mergeSort(a,0,n-1);
  
-  std::cout << "Sorted Array is:\n";
+  std::cout << "\nSorted Array is:\n";
   print(a, n);
- 
+  
+  std::cout << "\nNumber of times merge function was called: " << COUNT_MERGE_CALLS << "\n";
+  
+  int sum = 0;
+  for(int i=0; i<COUNT_MERGE_CALLS; i++) {
+    sum += COUNT_COMPARISONS[i];
+    std::cout << "No of insertions(comparisons) made in original array in merge " << i+1 << ": " << COUNT_COMPARISONS[i] << "\n";
+  }
+  std::cout << "Total no of comparisons made: " << sum << "\n";
   return 0;
 }
